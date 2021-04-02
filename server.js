393bbb3f62f8a11c9caf8dfcc3e33161bbefa8bd
jasperisaac27 <jasperisaac27@gmail.com>
2021-04-02 18:35:03 +0800

    Final

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const cartRoutes = require("./routes/cart.js");
 
 //Middlewares
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
